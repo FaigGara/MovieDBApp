f01f28468011faba27374631781407314b767f22
FaikKarazade <faik@mobisem.com>
2021-01-25 22:46:30 +0300

    Initial Commit

---
+++ MovieDBApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieDBApp
+//
+//  Created by Faiq on 25.01.2021.
+//  Copyright © 2021 Faig Garazade. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
