5a9845b6594f1a619dd907579eb05c94e6dc2fd7
FaikKarazade <faik@mobisem.com>
2021-01-28 12:50:31 +0300

    Movie List Application created.

---
+++ Classes/models/FavMovie+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  FavMovie+CoreDataClass.swift
+//  MovieDBApp
+//
+//  Created by Faiq on 28.01.2021.
+//  Copyright © 2021 Faig Garazade. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(FavMovie)
+public class FavMovie: NSManagedObject {
+
+}
---
+++ Classes/models/FavMovie+CoreDataProperties.swift
@@ -0,0 +1,26 @@
+//
+//  FavMovie+CoreDataProperties.swift
+//  MovieDBApp
+//
+//  Created by Faiq on 28.01.2021.
+//  Copyright © 2021 Faig Garazade. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension FavMovie {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<FavMovie> {
+        return NSFetchRequest<FavMovie>(entityName: "FavMovie")
+    }
+
+    @NSManaged public var db_id: Int64
+
+}
+
+extension FavMovie : Identifiable {
+
+}
